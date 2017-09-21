e58036e639eef4df9e70d112b24071c455f83a4e
Joe Churchman <churchman.joe@gmail.com>
2017-09-21 14:27:33 -0700

    delete garden redirects user to dashboard

--- src/gardenactionselector/GardenActionSelector.js
+++ src/gardenactionselector/GardenActionSelector.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 
 export default class GardenActionSelector extends Component {
     render (){
-        const { garden } = this.props;
+        const { garden, history } = this.props;
         const { saveGarden, deleteGarden } = this.props;
     
         return(
             <div>
                 <button type="button" onClick={() => saveGarden(garden)} >Save Garden</button>
-                <button type="button" onClick={() => deleteGarden(garden)} >Delete Garden</button>
+                <button type="button" onClick={() =>{
+                    deleteGarden(garden);
+                    history.push('/dashboard');
+                }} >Delete Garden</button>
             </div>
         );
     }
