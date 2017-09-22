dfa3ba2e360250f7b8e33af719ef19dde2612bbf
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2017-09-22 10:18:44 -0700

    dashboard links on own line, remove text from app.js, change terrible pun to "welcome"

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ class App extends Component {
     
         return (
             <div className="App">
-                Hello!
                 <DashboardContainer/>
             </div>
         );
