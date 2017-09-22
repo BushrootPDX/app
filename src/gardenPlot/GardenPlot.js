5d0c08149dafb1e25009b350745b2376b0603bf4
Mark Petrie <petrie.mark@gmail.com>
2017-09-21 23:10:42 -0700

    Start transition to react-dnd library

--- src/gardenPlot/GardenPlot.js
+++ src/gardenPlot/GardenPlot.js
@@ -1,45 +1,83 @@
-import React from 'react';
+import React, { Component } from 'react';
+import update from 'react/lib/update';
+import PropTypes from 'prop-types';
+import { DropTarget, DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+import ItemTypes from './ItemTypes';
 import styled from 'styled-components';
 
-export default function GardenPlot( props ) {
+const boxTarget = {
+    drop(props, monitor, component) {
+        const item = monitor.getItem();
+        const delta = monitor.getDifferenceFromInitialOffset();
+        const left = Math.round(item.left + delta.x);
+        const top = Math.round(item.top + delta.y);
 
-    const { garden, plotClicked, selectedPlant } = props;
-    const { _id, width, length, plot } = garden;
+        component.moveBox(item.id, left, top);
+    }
+};
 
-    const PlotDiv = styled.div`
+@DragDropContext(HTML5Backend)
+@DropTarget(ItemTypes.PLANT, boxTarget, connect => ({
+    connectDropTarget: connect.dropTarget()
+}))
+
+
+export default class GardenPLot extends Component {
+
+    moveBox(id, left, top) {
+        this.setState(update(this.state, {
+            boxes: {
+                [id]: {
+                    $merge: {
+                        left: left,
+                        top: top
+                    }
+                }
+            }
+        }));
+    }
+
+
+    render() {
+        const { garden, plotClicked, selectedPlant, hideSourceOnDrag, connectDropTarget } = this.props;
+        const { _id, width, length, plot } = garden;
+
+        const PlotDiv = styled.div`
         display: inline-block;
         border-style: solid;
         border-width: 2px;
         border-color: green;
     `;
 
-    return (
-        <PlotDiv>
-            <div
-                id={_id}
-                style={renderPlot(width, length)}
-                onClick={event => {                    
-                    const x = event.screenX;
-                    const y = event.screenY;
-                    if (event.target.id === _id) return plotClicked( garden, selectedPlant._id, x, y);
-                    plotClicked( _id, event.target.id, x, y);
-                }}>
-                {plot && Object.keys(plot).map(key => {
-                    const {instanceId, img, xPosition, yPosition, type} = plot[key];
+        return connectDropTarget(
+            <PlotDiv>
+                <div
+                    id={_id}
+                    style={renderPlot(width, length)}
+                    onClick={event => {                    
+                        const x = event.screenX;
+                        const y = event.screenY;
+                        if (event.target.id === _id) return plotClicked( garden, selectedPlant._id, x, y);
+                        plotClicked( _id, event.target.id, x, y);
+                    }}>
+                    {plot && Object.keys(plot).map(key => {
+                        const {instanceId, img, xPosition, yPosition, type} = plot[key];
 
-                    const xVal = xPosition;
-                    const yVal = yPosition;
-                    const transform = `translate( ${ xVal }px, ${ yVal }px)`;
+                        const xVal = xPosition;
+                        const yVal = yPosition;
+                        const transform = `translate( ${ xVal }px, ${ yVal }px)`;
 
-                    return <img
-                        id={instanceId}
-                        src={img}
-                        style={{transform}}
-                        alt={type} />;
-                })}
-            </div>
-        </PlotDiv>
-    );
+                        return <img
+                            id={instanceId}
+                            src={img}
+                            style={{transform}}
+                            alt={type} />;
+                    })}
+                </div>
+            </PlotDiv>
+        );
+    }
 }
 
 
